2e3de9d461794776b8fff1f386495da32721e2cc
Vinayak Neupane <92248047+Vineupane@users.noreply.github.com>
2022-01-24 21:32:13 +0100

    Update Plot 1.R

--- Plot 1.R
+++ Plot 1.R
@@ -1,11 +1,15 @@
-#exploratory data analysis project1(Plot 1).
+## Plot 1
+
 #load the data 
 df <- read.table("household_power_consumption.txt", header = T,
                  sep = ";", na.strings = "?")
+
 #change the date variable into date class
 df$Date <- as.Date(df$Date, format = "%d/%m/%Y")
+
 # subsetting the data
 sub_df<- subset(df, subset = (Date >= "2007-02-01" & Date <= "2007-02-02"))
+
 #convert date and time
 sub_df$datetime <- strptime(paste(sub_df$Date, sub_df$Time), "%Y-%m-%d %H:%M:%S")
 
